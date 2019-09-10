44d79fa1f443616700017872e6cc8e1cf8f1ff3f
NguyenDinhTrong99 <trongsad99@gmail.com>
2019-09-10 16:06:47 +0700

    Create menu in the bottom

---
+++ app/src/main/java/com/trongdeptrai/soundcloud/screen/home/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.trongdeptrai.soundcloud.screen.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import com.trongdeptrai.soundcloud.R;
+import com.trongdeptrai.soundcloud.screen.BaseFragment;
+
+public class HomeFragment extends BaseFragment {
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void initView(View v) {
+    }
+
+    @Override
+    public void initData() {
+    }
+
+    @Override
+    public void initListener() {
+    }
+}
